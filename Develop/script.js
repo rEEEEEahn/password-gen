9cb47728aaa12c022af9c0c901d1a810ccd7a350
Ryan Hsiao <rdjr.hsiao@gmail.com>
2021-09-23 18:03:50 -0700

    added 'please select at least one' alert

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var numbers = [0, 1, 2, 3, 4 , 5, 6, 7, 8, 9,];
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
-var upperA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var special = "!@#$%^&*()"
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var upperA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$%^&*()";
 
 var userLength = 0;
 var userLower = false;
@@ -22,15 +22,15 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  userLength
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
+// add confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 //prom0pt question
 function passwordQuestion () {
-  var text;
+
   var userLength = prompt("Please choose password length between 8-128 characters.");
   if (!userLength) {
     alert("Have a good day!");
@@ -38,47 +38,46 @@ function passwordQuestion () {
   }
   if (userLength <8 || userLength >128) {
     prompt("Please choose password length between 8-128 characters.");
-    passwordQuestion()
+    passwordQuestion();
   }
   else lowerCaseQuestion ();
 }
 //lowercase question
 function lowerCaseQuestion () {
-  var text;
-  var userLower = confirm("Will your password contain lowercases?");
+
+  userLower = confirm("Will your password contain lowercases?");
     upperCaseQuestion();
 }
 
-
 //uppercase
 function upperCaseQuestion () {
-  var text;
-  var userUpper = confirm("Will your password contain uppercases?");
+  userUpper = confirm("Will your password contain uppercases?");
     numericQuestion();
 }
 
 
 //numeric
 function numericQuestion () {
-  var text;
-  var userNumber = confirm("Will your password contain numbers?");
+  userNumber = confirm("Will your password contain numbers?");
     specialQuestion();
 }
 
 //special character
 function specialQuestion () {
-  var text;
-  var userSpecial = confirm("Will your password contain special characters?");
-  if (specialQuestion === true) {
-    writePassword();
+  userSpecial = confirm("Will your password contain special characters?");
+    condition();
 }
-else {
-  alert("Please select at least one!");
-  passwordQuestion ();
-}
-}
+ function condition () {
+   if (userLower === false && userUpper === false && userNumber === false && userSpecial === false) {
+    alert("Please select at least one!");
+    lowerCaseQuestion ();
 
-// add confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  }else writePassword(); 
+ }
+
+
+
+// add confirm whether or not to include lowercase, uppercase, numeric, and/or special characters !userLower && !userUpper && !userNumber && !userSpecial
 
 
 // Add event listener to generate button
