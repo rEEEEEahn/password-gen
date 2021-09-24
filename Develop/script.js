68086c0e96a80f31d0b85b23504fae64d14aeb02
Ryan Hsiao <rdjr.hsiao@gmail.com>
2021-09-23 21:37:25 -0700

    finish product and readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var userLength = 0
+
+var length = 0
 var userLower = false
 var userUpper = false
 var userNum = false
@@ -20,31 +21,67 @@ var prequisite = 0
     if (length < 8 ||length > 128) {
       alert("Please choose password length between 8-128 characters.")
       check()
-    } else if (!Number.isInteger(length)) {
-      alert('Only numbers please!')
-      check()
     }
   }
   function passwordQuestion(){
-    lowerCase = confirm('Do you need Lower Case letters?')
-    upperCase = confirm('Do you need Upper Case letters?')
-    nums = confirm('Do you need numbers?')
-    specChars = confirm('Do you need special characters?')
-    if (!lowerCase && !upperCase && !specChars && !nums) {
+    userLower = confirm('Do you need Lower Case letters?')
+    userUpper = confirm('Do you need Upper Case letters?')
+    userNum = confirm('Do you need numbers?')
+    userSpecial = confirm('Do you need special characters?')
+    if (!userLower && !userUpper && !userNum && !userSpecial) {
       alert('Invalid input, please choose at least one parameter!')
       passwordQuestion()
     }
   }
 
-
+ 
   // add confirm whether or not to include lowercase, uppercase, numeric, and/or special characters !userLower && !userUpper && !userNumber && !userSpecial
 
 
   // Add event listener to generate button
+  document.getElementById('generate').addEventListener('click', function () {
 
-  // Checks includes adding array to chosen array and adding prequisite characters
+    check()
+    passwordQuestion()
+    chosenArray = ""
+    password = ""
+    prequisite = 0
+
+    if (!userLower && !userUpper&& !userNum&& !userSpecial) {
+      alert("You must choose at least one parameter")      
+      location.reload()
+    }
   // lowerCase check
+  if (userLower) {
+    prequisite++
+    array += lower
+    password += lower[Math.floor(Math.random() * lower.length)]
+  }
   // upper check
+  if (userUpper) {
+    prequisite++
+    array += upper
+    password += upper[Math.floor(Math.random() * upper.length)]
+  }
   // number check
+  if (userNum) {
+    prequisite++
+    array += number
+    password += number[Math.floor(Math.random() * number.length)]
+  }
   // spec check
+  if (userSpecial) {
+    prequisite++
+    array += special
+    password += special[Math.floor(Math.random() * special.length)]
+  }
+
+  length -= prequisite
+
   // password generation loop
+  for (let i = 0; i < length; i++) {
+    password += array[Math.floor(Math.random() * array.length)]
+  }
+  console.log(password)
+  document.getElementById('password').innerHTML = password
+})
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# password-gen
+# <Password Generator>
+## Description
+Homework 3 is all about javascript. my task was to write the javascript for a password generator; html and css already provided.
+## Installation
+Writing the javascript was tough for me. I had to really depend on google and past classwork to get through this. I have to admit, I did restart towards the end of my first try because i just could nto get the generator to work. I figured that it might be easier to restart instead of trying to debug the code. psuedocode was very helpful as well to get things rolling.
+## Usage
+![page](./password-gen/Assets/page.png)
+Password Generator. HTML and CSS provided.
+![prompt](./password-gen/Assets/prompt.png)
+prompt will pop up and password question will appear. ex: would you like uppercase in the password.
+![password](./password-gen/Assets/password.png)
+End result!
+
+## License
+Copyright (c) [2021] [Ryan Hsiao]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
